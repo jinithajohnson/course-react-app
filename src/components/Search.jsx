dcae544be58dbf3c414de58442a66d89987cb6ff
jinithajohnson <jinithajohnson02@gmail.com>
2024-06-12 13:33:43 +0530

    success

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
+import axios from 'axios'
 
 const Search = () => {
+    const [data, changeData] = useState(
+       {
+        title:""
+       }
+        
+    )
+    const [result,changeResult] = useState([])
+    const inputHandler = (event) => {
+        changeData({...data, [event.target.name]:event.target.value})
+    }
+        
+    const readValue = () => {
+        console.log(data)
+        axios.post("http://localhost:8080/search",data).then(
+            (response)=>{
+                
+                changeResult(response.data)
+           }
+
+        ).catch(
+            (error)=>{
+                console.log(error)
+            }
+        )
+    }
+       
   return (
     <div>
         <NavBar/>
@@ -11,17 +38,58 @@ const Search = () => {
                             <div className="row">
                                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                     <label htmlFor="" className="form-label">title</label>
-                                    <input type="text" className="form-control" />
+                                    <input type="text" className="form-control" name='title' value={data.title} onChange={inputHandler}/>
                                 </div>
                                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                    <button className="btn btn-primary">search</button>
+                                    <button className="btn btn-primary" onClick={readValue}>search</button>
                                 </div>
                             </div>
 
                     </div>
                 </div>
             </div>
+<div className="row">
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xll-12">
 
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">title</th>
+      <th scope="col">description</th>
+      <th scope="col">duration</th>
+      <th scope="col">date</th>
+      <th scope="col">venue</th>
+      <th scope="col">tname</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    {
+        result.map(
+            (value,index)=>{
+                return <tr>
+                <td>{value.title}</td>
+                <td>{value.description}</td>
+                <td>{value.duration}</td>
+                <td>{value.date}</td>
+                <td>{value.venue}</td>
+                <td>{value.tname}</td>
+                <td>
+                    <button className="btn btn-danger">Delete</button>
+                </td>
+
+                
+              </tr>
+            }
+        )
+    }
+    
+    
+  </tbody>
+</table>
+
+    </div>
+</div>
 
     </div>
   )
